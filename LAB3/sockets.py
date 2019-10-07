acfe7e210926d547e8a210ec91beafaa5ebb5650
mrTiisetso <Nthulitiisetso@gmail.com>
2019-10-07 13:10:09 +0200

    25224549 -Python socket created

---
+++ LAB3/sockets.py
@@ -0,0 +1,14 @@
+1  import socket
+2  import sys
+3
+4  # Create socket using the following 
+5
+6  def socket_create():
+7      try:
+8          global host
+9          global port 
+10         global s
+11         host = ''
+12         port = 9999
+13
+
